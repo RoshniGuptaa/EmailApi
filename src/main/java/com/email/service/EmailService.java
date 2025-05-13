2df89a47a01904c072023460f40f43ea4da1a7eb
RoshniGuptaa <128632897+RoshniGuptaa@users.noreply.github.com>
2025-05-13 09:50:57 +0530

    Adding api project

---
+++ src/main/java/com/email/service/EmailService.java
@@ -0,0 +1,72 @@
+package com.email.service;
+
+import java.util.Properties;
+import jakarta.mail.PasswordAuthentication;
+
+import java.io.File;
+
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+//import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+//import jakarta.mail.internet.MimeMultipart;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	//This method is responsible to send email
+		public  boolean sendEmail(String message, String subject, String to) {
+			// TODO Auto-generated method stub
+			boolean f=false;
+			String from="roshnigupta202003@gmail.com";
+
+			//Variable for gmail
+			String host="smtp.gmail.com";
+			
+			//Get system properties
+			Properties properties = System.getProperties();
+			System.out.println("PROPERTIES :"+properties);
+			
+			//Setting important information to properties object
+			
+			//host set
+			properties.put("mail.smtp.host",host);
+			properties.put("mail.smtp.port", "465");
+			properties.put("mail.smtp.ssl.enable", "true");
+			properties.put("mail.smtp.auth", "true");
+			
+			//Step1 : To get session object
+			Session session=Session.getInstance(properties,new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("roshnigupta202003@gmail.com","opsppdqcioltbwnb" );
+				}
+				
+			});
+			
+			session.setDebug(true);
+			//Step 2:compose the message
+			MimeMessage m=new MimeMessage(session);
+			
+			try {
+				m.setFrom(from);
+				m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				m.setSubject(subject);
+				m.setText(message);
+				
+				//Step 3 :send message using Transport class
+				Transport.send(m);
+				System.out.println("Mail sent .Success..........");
+				f=true;
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			return f;
+		}
+}
